9b04b360e4e81b868fef36f113b49b492eaf8a96
safanaarsila17 <safanaarsila17@gmail.com>
2021-09-20 18:58:21 +0700

    pertemuan1

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+ public function index()
+ {
+ echo "<h1>Perkenalkan</h1>";
+ echo"Nama saya Safana arsila rachmaini
+ Saya tingga di daerah kampung melayu 
+ olah raga yang saya sukai adalah
+ Bulutangkis";
+ }
+}
