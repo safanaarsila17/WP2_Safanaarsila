a512e68487d508961a9e9946f4bc589c76c28383
safanaarsila17 <safanaarsila17@gmail.com>
2021-09-22 05:26:07 +0700

    pertemuan4

---
+++ application/views/v_about.php
@@ -0,0 +1,23 @@
+<section>
+  <h1><?php echo $judul ?></h1>
+  <h4>Nama</h4>
+  <ul type="disc">
+    <li>Nama Depan : Safana </li>
+    <li>Nama Belakang : Arsila</li>
+  </ul>
+  <br>
+  <h4>Alamat</h4>
+  <ul type="none">
+    <li>Jalan Kebon Pala Tanah Rendah DKI Jakarta</li>
+  </ul>
+
+  <h4>Tempat Lahir</h4>
+  <ul type="none">
+    <li>Jakarta</li>
+  </ul>
+
+  <h4>Olahraga Favorit </h4>
+  <ul type="square">
+    <li>Bulutangkis</li>
+  </ul>
+</section> 
